514bcfbe886dd683e2d859cd430baf6482df0c58
ef1301 <fangemily60@gmail.com>
2020-11-21 15:08:19 -0500

    users

---
+++ users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users VALUES (0, 'cake', 'cake@email.com', 'cakesaregreat');
+INSERT INTO users VALUES (1, 'fruittart', 'ftart@email.com', 'ihatedurian123');
+INSERT INTO users VALUES (2, 'pretzel', 'pretzel@email.com', 'ilovepretzels548');
+INSERT INTO users VALUES (3, 'eaJPacman', 'eaJ@gmail.com', 'day6eaJ');
+INSERT INTO users VALUES (4, 'sushi', 'sushi@yahoo.com', 'Salmon097');
