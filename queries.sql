1cad74226e0ce80d906c14b2e49a0f7275be88fa
ef1301 <fangemily60@gmail.com>
2020-12-02 18:11:47 -0500

    queries

--- queries.sql
+++ queries.sql
@@ -1,7 +1,5 @@
---SELECT anime.title, COUNT(genre_id) AS genre_count from anime INNER JOIN genre_to USING(anime_id) GROUP BY anime_id ORDER BY anime_id;
---SELECT title, AVG(rating) AS anime_rating from anime INNER JOIN anime_list USING(anime_id) GROUP BY anime_id ORDER BY anime_rating DESC LIMIT 1;
-
-SELECT MIN(rating) AS anime_rating from anime_list;
+--SELECT MIN(rating) AS anime_rating from anime_list;
+select username, max(rating) from anime_list inner join users using (user_id) group by user_id, username;
 
 SELECT title, COUNT(anime_id) AS anime_count from anime INNER JOIN anime_list USING(anime_id) GROUP BY anime_id ORDER BY anime_count DESC LIMIT 10;
 
